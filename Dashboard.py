22a37b6fb04be6596816fd1f08c7cc2ad02628a5
Pedro <pedrohbustamante@icloud.com>
2024-10-16 10:44:32 -0300

    names

--- Dashboard.py
+++ Dashboard.py
@@ -57,7 +57,7 @@ def main() -> None:
         
         # Criar um DataFrame a partir dos resultados e nomes de colunas
         intimacoes_dados = pd.DataFrame(intimacoes, columns=colunas)
-        intimacoes_dados = intimacoes_dados['name' in names]
+        intimacoes_dados = intimacoes_dados[citacoes_dados['name'] in names]
 
         cursor = conn.cursor()
         # Executar a consulta SQL
