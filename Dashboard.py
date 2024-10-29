b108010af9521d1463f28929d9a505a229469360
Pedro <pedrohbustamante@icloud.com>
2024-10-29 09:33:34 -0300

    k

--- Dashboard.py
+++ Dashboard.py
@@ -1,39 +1,94 @@
 import datetime
+import os
+import dotenv
 import pandas as pd
 import plotly.express as px
+import pymysql
+import streamlit as st
 from pyecharts.charts import Bar
 from pyecharts import options as opts
-import streamlit as st
 from streamlit_echarts import st_pyecharts
 
-# Código principal
-def main():
-    try:
-        # Dados de exemplo para citações e intimações
-        citacoes_dados = pd.DataFrame(...)  # Substitua pelos dados reais
-        intimacoes_dados = pd.DataFrame(...)  # Substitua pelos dados reais
+st.set_page_config(layout="wide")
 
+# Título da página
+st.title("Estatísticas da Procuradoria Trabalhista - PTB :bar_chart:")
+
+dotenv.load_dotenv()
+
+# Informações de conexão com o banco de dados
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD") or ""
+database = os.getenv("DB_DATABASE")
+
+def main() -> None:
+    try:
+        if not host or not user or not database:
+            st.error("Faltam informações de conexão com o banco de dados.")
+            return
+
+        conn = pymysql.connect(
+            host=host,
+            port=3306,
+            user=user,
+            password=password,
+            database=database,
+        )
+        
+        names = [
+            'Natália Franco Massuia e Marcondes',
+            'Anamaria Barbosa Ebram Fernandes',
+            'Leonardo Tokuda Pereira',
+            'Leonardo Warmling Candido da Silva',
+            'Marcelo Moura da Silva',
+            'Pedro Carvalho Mitre Chaves',
+            'Kelly Cristina Majima'
+        ]
+        
         tabs = st.tabs(["Citações", "Intimações"])
+        
+        cursor = conn.cursor()
+        
+        # Consulta para citações
+        cursor.execute(
+            "SELECT * FROM ANDAMENTOS WHERE nome_procuradoria='PTB' AND natureza LIKE 'cit%' COLLATE utf8mb4_unicode_ci",
+        )
+        citacoes = cursor.fetchall()
+        colunas = [desc[0] for desc in cursor.description]
+        citacoes_dados = pd.DataFrame(citacoes, columns=colunas)
+        citacoes_dados = citacoes_dados[citacoes_dados['name'].isin(names)]
+
+        # Consulta para intimações
+        cursor.execute(
+            "SELECT * FROM ANDAMENTOS WHERE nome_procuradoria='PTB' AND natureza LIKE 'int%' COLLATE utf8mb4_unicode_ci",
+        )
+        intimacoes = cursor.fetchall()
+        colunas = [desc[0] for desc in cursor.description]
+        intimacoes_dados = pd.DataFrame(intimacoes, columns=colunas)
+        intimacoes_dados = intimacoes_dados[intimacoes_dados['name'].isin(names)]
+
+        conn.close()
 
         # Filtro de data para citações
         with tabs[0]:
             dados = citacoes_dados
-
+            
             with st.sidebar:
                 st.subheader("Filtro de Data (Citações)")
-                start_date, end_date = st.date_input("Selecione o intervalo de datas:", [datetime.datetime(2024,5,15), datetime.datetime.today()], key='cit_date_input')
+                start_date, end_date = st.date_input("Selecione o intervalo de datas:", [datetime.datetime(2024,5,15),datetime.datetime.today()], key='cit_date_input')
                 print(start_date)
 
             # Verificar se a coluna 'datapub' existe antes de aplicar o filtro
+           
             st.header("Filtro de Data")
             if 'datapub' in dados.columns:
                 dados["datapub"] = pd.to_datetime(dados["datapub"])
                 if start_date and end_date:
-                    dados = dados[(dados['datapub'] >= datetime.datetime(start_date.year, start_date.month, start_date.day)) & 
-                                  (dados['datapub'] <= datetime.datetime(end_date.year, end_date.month, end_date.day))]
+                    dados = dados[(dados['datapub'] >= datetime.datetime(start_date.year,start_date.month,start_date.day)) & (dados['datapub'] <= datetime.datetime(end_date.year,end_date.month,end_date.day))]
             else:
                 st.warning("A coluna 'datapub' não foi encontrada nos dados de citações.")
-
+            
             total_publicacoes = len(dados)
             st.metric(label="Quantidade Total", value=total_publicacoes)
 
@@ -68,7 +123,7 @@ def main():
                         toolbox_opts=opts.ToolboxOpts(),
                     )
                 )
-
+                
                 col1, col2 = st.columns(2)
                 with col1:
                     st.plotly_chart(fig_pizza, height=500)
@@ -93,22 +148,19 @@ def main():
         # Filtro de data para intimações
         with tabs[1]:
             dados = intimacoes_dados
-
+            
             with st.sidebar:
                 st.subheader("Filtro de Data (Intimações)")
-                start_date, end_date = st.date_input("Selecione o intervalo de datas:", [datetime.datetime(2024,5,15), datetime.datetime.today()], key='int_date_input')
+                start_date, end_date = st.date_input("Selecione o intervalo de datas:", [datetime.datetime(2024,5,15),datetime.datetime.today()], key='int_date_input')
                 print(start_date)
 
-            # Verificar se a coluna 'datapub' existe antes de aplicar o filtro
-            st.header("Filtro de Data")
             if 'datapub' in dados.columns:
                 dados["datapub"] = pd.to_datetime(dados["datapub"])
                 if start_date and end_date:
-                    dados = dados[(dados['datapub'] >= datetime.datetime(start_date.year, start_date.month, start_date.day)) & 
-                                  (dados['datapub'] <= datetime.datetime(end_date.year, end_date.month, end_date.day))]
+                    dados = dados[(dados['datapub'] >= start_date) & (dados['datapub'] <= end_date)]
             else:
                 st.warning("A coluna 'datapub' não foi encontrada nos dados de intimações.")
-
+            
             total_publicacoes = len(dados)
             st.metric(label="Quantidade Total", value=total_publicacoes)
 
@@ -148,7 +200,7 @@ def main():
                 with col1:
                     st.plotly_chart(fig_pizza, height=500)
                 with col2:
-                    st_pyecharts(bar, key="echarts_intimacoes")
+                    st_pyecharts(bar)
 
                 fig_barras_plotly = px.bar(
                     publicacoes_mensais,
