95019320798a54a3638e8b80cfd95ae73b67bdbe
Pedro <pedrohbustamante@icloud.com>
2024-11-04 09:47:25 -0300

    teste gap

--- Dashboard.py
+++ Dashboard.py
@@ -293,6 +293,7 @@ def main() -> None:
                         title_opts=opts.TitleOpts(
                             title="Publicações Mensais (Intimações)",
                             subtitle="Total por mês",
+                            title_gap=10
                         ),
                         toolbox_opts=opts.ToolboxOpts(),
                     )
